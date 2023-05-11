b044e2b61bb4d9f15541c92aec4afa2cb2f6fb30
Timur <kuromukade@mail.ru>
2023-05-11 21:06:00 +0300

    split scss and modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,33 +1,35 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  const { isDev } = options;
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // dont compile css if dev
-            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
 
-            // setup css modules
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: true
-                }
-            },
-            "sass-loader",
-        ],
-    };
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // dont compile css if dev
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
 
-    return [
-        typescriptLoader,
-        cssLoader,
-    ]
+      // setup css modules
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base:64:5]' : '[hash:base64:8]',
+            auto: (resPath: string) => resPath.includes('.module.'),
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
+
+  return [typescriptLoader, cssLoader];
 }
