f88453666bed0e3b21fac49903678d2cff81a5ec
Timur <kuromukade@mail.ru>
2023-05-11 21:52:42 +0300

    add routes, code splitting with react lazy and suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Counter } from "./components/Counter"
+import { Suspense } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { AboutPageAsync } from './components/AboutPage/AboutPage.async';
+import { MainPageAsync } from './components/MainPage/MainPage.async';
 
 import './index.scss';
 
 const App = () => {
-    return (
-        <div className="app">
-            <Counter />
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Link to={'/about'}>About</Link>
+      <Link to={'/'}>Home</Link>
 
-export default App
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
