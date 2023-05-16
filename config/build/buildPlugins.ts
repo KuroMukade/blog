752f5a1cf89e2b170ff839283f6ebecd6e9d3619
Timur <kuromukade@mail.ru>
2023-05-16 23:12:34 +0300

    add hmr, add webpack bundle analyzer, moved jest packege to dev dependencies

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { BuildOptions } from './types/config';
 
@@ -20,5 +21,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new BundleAnalyzerPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ].filter(Boolean);
 }
