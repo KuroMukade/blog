e2aab2dfa3da289472242ad724f2c52541a5a347
kuromukade <kuromukade@mail.ru>
2025-03-14 13:28:32 +0400

    remove strange package && remove conosle log

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -24,7 +24,6 @@ export const Navbar = ({ onBurgerClick }: NavbarProps) => {
   const dispatch = useDispatch();
 
   const authData = useSelector(getUserAuthData);
-  console.log({ authData });
 
   const onToggleModal = useCallback(() => {
     setAuthOpen((prev) => !prev);
